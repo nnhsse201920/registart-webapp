ff3e19aa45a61891f10b3127ff5811a7960b1bec
Tom Carsello <tkcarsello@gmail.com>
2020-01-13 13:55:52 -0600

    Created basic Flask server

---
+++ Project/app/routes.py
@@ -0,0 +1,15 @@
+from app import app
+from flask import render_template
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template('index.html')
+
+@app.route('/survey')
+def survey():
+    return render_template('survey.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
