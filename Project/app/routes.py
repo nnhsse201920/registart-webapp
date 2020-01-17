dde8354963bc7c2621ece60bfe57c583662f66e7
Luke Zhang <lukezhangxc@gmail.com>
2020-01-17 13:39:37 -0600

    Started groundwork for login page

--- Project/app/routes.py
+++ Project/app/routes.py
@@ -1,5 +1,8 @@
+from flask import render_template, flash, redirect, url_for, request
+from flask_login import login_user, logout_user, current_user, login_required
+from werkzeug.urls import url_parse
 from app import app
-from flask import render_template
+from app.forms import LoginForm, RegistrationForm, EditProfileForm, PostForm
 
 @app.route('/')
 @app.route('/index')
@@ -13,3 +16,8 @@ def survey():
 @app.route('/login')
 def login():
     return render_template('login.html', title='Login')
+
+@app.route('/register')
+def register():
+
+    return render_template('register.html',title='Create Account')
