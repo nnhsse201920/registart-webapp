7657a7348540ec354743ba733c327c643bfea331
Luke Zhang <lukezhangxc@gmail.com>
2020-01-17 14:14:12 -0600

    need to debug

--- Project/app/routes.py
+++ Project/app/routes.py
@@ -2,12 +2,12 @@ from flask import render_template, flash, redirect, url_for, request
 from flask_login import login_user, logout_user, current_user, login_required
 from werkzeug.urls import url_parse
 from app import app
-from app.forms import LoginForm, RegistrationForm, EditProfileForm, PostForm
+from app.forms import LoginForm, RegistrationForm
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.html')
+    return render_template('index.html',current_user=current_user)
 
 @app.route('/survey')
 def survey():
@@ -15,9 +15,29 @@ def survey():
 
 @app.route('/login')
 def login():
-    return render_template('login.html', title='Login')
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('index')
+        return redirect(next_page)
+    return render_template('login.html', title='Login', form=form)
 
-@app.route('/register')
-def register():
-
-    return render_template('register.html',title='Create Account')
+@app.route('/register', methods=['GET','POST'])
+def register(): 
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        flash('Account successfully created')
+        return redirect(url_for('login'))    
+    return render_template('login.html',title='Create Account', form=form)
