7a8f9f01f40c45f0a2b2c64bb5ed7ed078a00a38
jamesjhuang12 <35243958+jamesjhuang12@users.noreply.github.com>
2020-02-07 14:14:37 -0600

    Trying to connect to database

--- Project/config.py
+++ Project/config.py
@@ -5,6 +5,5 @@ load_dotenv(os.path.join(basedir, '.env'))
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
