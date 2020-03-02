b1423feb0e2b00d1e48effb008baf0eabfc84f70
Luke Zhang <38232741+lukezhang31@users.noreply.github.com>
2020-03-02 12:44:08 -0600

    Update config.py

--- Project/config.py
+++ Project/config.py
@@ -5,7 +5,6 @@ load_dotenv(os.path.join(basedir, '.env'))
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     CSRF_ENABLED = True
