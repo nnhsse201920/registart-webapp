020c621e91e5c88287c1997466d8087bbf268954
Luke Zhang <lukezhangxc@gmail.com>
2020-03-01 22:39:33 -0600

    blurred cover photo for intro page, working on landing page

--- Project/app/routes.py
+++ Project/app/routes.py
@@ -12,7 +12,7 @@ from app.models import *
 def index():
     if current_user.is_anonymous:
         return redirect(url_for('login'))
-    return render_template('index.html',current_user=current_user,isOnSurvey=False)
+    return render_template('index.html',current_user=current_user)
 
 @app.route('/survey')
 def survey():
@@ -21,7 +21,7 @@ def survey():
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if current_user.is_authenticated:
-        return redirect(url_for('about'))
+        return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():
         user = Organizers.query.filter_by(username=form.username.data).first()
@@ -31,19 +31,19 @@ def login():
         login_user(user, remember=form.remember_me.data)
         next_page = request.args.get('next')
         if not next_page or url_parse(next_page).netloc != '':
-            next_page = url_for('about')
+            next_page = url_for('index')
         return redirect(next_page)
     return render_template('login.html', title='Sign In', form=form)
 
 @app.route('/logout')
 def logout():
     logout_user()
-    return redirect(url_for('index'))
+    return redirect(url_for('about'))
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     if current_user.is_authenticated:
-        return redirect(url_for('about'))
+        return redirect(url_for('index'))
     form = RegistrationForm()
     if form.validate_on_submit():
         user = Organizers(firstN=form.firstname.data, lastN=form.lastname.data,username=form.username.data, email=form.email.data)
@@ -51,17 +51,9 @@ def register():
         db.session.add(user)
         db.session.commit()
         login_user(user)
-        return redirect(url_for('about'))
+        return redirect(url_for('index'))
     return render_template('register.html', title='Register', form=form)
  
-
-@app.route('/survey/about',methods=['GET', 'POST'])
-@login_required
-def about():
-    if current_user.is_anonymous:
-        return redirect(url_for('login'))
-    flash('Please complete this step before proceeding.')
-    return render_template('about.html', title='About', isOnSurvey=True)
 
 @app.route('/survey/activities',methods=['GET', 'POST'])
 @login_required
@@ -71,8 +63,7 @@ def activities():
     form = ActivitiesForm()
     if form.validate_on_submit():
         return redirect(url_for('connections'))
-    flash('Please complete this step before proceeding.')
-    return render_template('activities.html', title='Your Activities', form=form,isOnSurvey=True)
+    return render_template('activities.html', title='Your Activities', form=form)
 
 @app.route('/survey/connections',methods=['GET', 'POST'])
 @login_required
@@ -83,5 +74,8 @@ def connections():
     
     if form.validate_on_submit():
         return redirect(url_for('index'))
-    flash('Please complete this step before proceeding.')
-    return render_template('connections.html', title='Registart | Connections', isOnSurvey=True,form=form)
+    return render_template('connections.html', title='Registart | Connections',form=form)
+
+@app.route('/about',methods=['GET', 'POST'])
+def about():
+    return render_template('about.html', title='Registart | About')
