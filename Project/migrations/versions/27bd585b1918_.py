52fa606f34179be2afa52f47d05e7439f6280671
Luke Zhang <lukezhangxc@gmail.com>
2020-04-23 16:50:53 -0500

    Initital commit

--- Project/migrations/versions/27bd585b1918_.py
+++ Project/migrations/versions/27bd585b1918_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: 60550656445b
+Revision ID: 27bd585b1918
 Revises: 
-Create Date: 2020-04-23 14:14:27.090454
+Create Date: 2020-04-23 16:45:28.730417
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '60550656445b'
+revision = '27bd585b1918'
 down_revision = None
 branch_labels = None
 depends_on = None
