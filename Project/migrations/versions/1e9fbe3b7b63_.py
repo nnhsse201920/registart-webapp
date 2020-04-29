a2a4477cd1ac5f90b53b8c806c26c5d40f84b7ad
Luke Zhang <lukezhangxc@gmail.com>
2020-04-29 12:38:09 -0500

    backend for relationship rankings

---
+++ Project/migrations/versions/1e9fbe3b7b63_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 1e9fbe3b7b63
+Revises: 1e9fbe3b763
+Create Date: 2020-04-29 11:43:19.143589
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '1e9fbe3b7b63'
+down_revision = '1e9fbe3b763'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
