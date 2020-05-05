8bd82c3e61d941f40759faeb78210769cd13083b
Luke Zhang <lukezhangxc@gmail.com>
2020-05-04 20:52:55 -0500

    Updated init.sql for demo

--- db/init.sql
+++ db/init.sql
@@ -1,18 +1,15 @@
--- MariaDB dump 10.17  Distrib 10.4.11-MariaDB, for debian-linux-gnu (x86_64)
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
 --
--- Host: localhost    Database: example
--- ------------------------------------------------------
--- Server version	10.4.11-MariaDB-1:10.4.11+maria~bionic
+-- Host: db
+-- Generation Time: May 05, 2020 at 01:52 AM
+-- Server version: 8.0.20
+-- PHP Version: 7.4.5
 
-
---
--- Table structure for table assignments'
---
-DROP DATABASE IF EXISTS registart;
-CREATE DATABASE registart;
-CREATE USER 'registart'@'%' identified by 'database7';
-GRANT ALL PRIVILEGES on registart.* to 'registart'@'%';
-use registart;
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
@@ -31,7 +28,7 @@ use registart;
 --
 
 CREATE TABLE `activity` (
-  `id` int(11) NOT NULL,
+  `id` int NOT NULL,
   `name` varchar(64) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -42,14 +39,19 @@ CREATE TABLE `activity` (
 INSERT INTO `activity` (`id`, `name`) VALUES
 (5, 'Boy\'s Basketball'),
 (1, 'Boy\'s Cross Country'),
+(12, 'Boy\'s Gymnastics'),
 (2, 'Boy\'s Track and Field'),
+(13, 'Chess Team'),
 (3, 'Computer Science Club'),
 (11, 'DECA'),
 (10, 'eSports Team'),
+(16, 'Mandarin Club'),
 (6, 'Math Team'),
 (7, 'National Honor Society'),
+(15, 'Ping Pong Club'),
 (4, 'Robotics'),
-(8, 'Spanish National Honor Society');
+(8, 'Spanish National Honor Society'),
+(14, 'Table Tennis Team');
 
 -- --------------------------------------------------------
 
@@ -66,7 +68,7 @@ CREATE TABLE `alembic_version` (
 --
 
 INSERT INTO `alembic_version` (`version_num`) VALUES
-('28095aae65f8');
+('45716b484161');
 
 -- --------------------------------------------------------
 
@@ -75,11 +77,11 @@ INSERT INTO `alembic_version` (`version_num`) VALUES
 --
 
 CREATE TABLE `assignments` (
-  `organizerID` int(11) NOT NULL,
-  `studentID` int(11) DEFAULT NULL,
+  `organizerID` int NOT NULL,
+  `studentID` int DEFAULT NULL,
   `lastContact` datetime DEFAULT NULL,
-  `status` int(11) DEFAULT NULL,
-  `medium` int(11) DEFAULT NULL
+  `status` int DEFAULT NULL,
+  `medium` int DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -89,11 +91,11 @@ CREATE TABLE `assignments` (
 --
 
 CREATE TABLE `connection` (
-  `id` int(11) NOT NULL,
+  `id` int NOT NULL,
   `firstN` varchar(64) DEFAULT NULL,
   `lastN` varchar(64) DEFAULT NULL,
-  `ranking` smallint(6) DEFAULT NULL,
-  `user_id` int(11) DEFAULT NULL
+  `ranking` smallint DEFAULT NULL,
+  `user_id` int DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -101,27 +103,14 @@ CREATE TABLE `connection` (
 --
 
 INSERT INTO `connection` (`id`, `firstN`, `lastN`, `ranking`, `user_id`) VALUES
-(82, 'Aditya', 'Tolia', NULL, 3),
-(83, 'Ishaan', 'Mathur', NULL, 3),
-(84, 'Jack', 'Cook', NULL, 3),
-(85, 'Leo', 'Oberman', NULL, 3),
-(86, 'Robert', 'Azarcon', NULL, 3),
-(87, 'Vijay', 'Mistry', NULL, 3),
-(88, 'Yinuo', 'Qin', NULL, 3),
-(89, 'Aditya', 'Tolia', NULL, 2),
-(90, 'Ishaan', 'Mathur', NULL, 2),
-(91, 'Robert', 'Azarcon', NULL, 2),
-(92, 'Vijay', 'Mistry', NULL, 2),
-(93, 'Yinuo', 'Qin', NULL, 2),
-(94, 'Aditya', 'Tolia', NULL, 1),
-(95, 'Ethan', 'Pulvermacher', NULL, 1),
-(96, 'Ishaan', 'Mathur', NULL, 1),
-(97, 'Jack', 'Cook', NULL, 1),
-(98, 'Leo', 'Oberman', NULL, 1),
-(99, 'Nicholas', 'Williams', NULL, 1),
-(100, 'Robert', 'Azarcon', NULL, 1),
-(101, 'Vijay', 'Mistry', NULL, 1),
-(102, 'Yinuo', 'Qin', NULL, 1);
+(103, 'Enoch', 'Qin', 4, 5),
+(104, 'Ethan', 'He', 2, 5),
+(105, 'Ishaan', 'Mathur', 3, 5),
+(106, 'James', 'Huang', 4, 5),
+(108, 'Joshua', 'Robinson', 3, 5),
+(109, 'Omeed', 'Jamali', 4, 5),
+(110, 'Tom', 'Carsello', 3, 5),
+(111, 'Yinuo', 'Qin', 4, 5);
 
 -- --------------------------------------------------------
 
@@ -130,11 +119,11 @@ INSERT INTO `connection` (`id`, `firstN`, `lastN`, `ranking`, `user_id`) VALUES
 --
 
 CREATE TABLE `organizers` (
-  `id` int(11) NOT NULL,
+  `id` int NOT NULL,
   `firstN` varchar(255) DEFAULT NULL,
   `lastN` varchar(255) DEFAULT NULL,
   `username` varchar(255) DEFAULT NULL,
-  `organizationID` int(11) DEFAULT NULL,
+  `organizationID` int DEFAULT NULL,
   `email` varchar(255) DEFAULT NULL,
   `password` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -144,10 +133,7 @@ CREATE TABLE `organizers` (
 --
 
 INSERT INTO `organizers` (`id`, `firstN`, `lastN`, `username`, `organizationID`, `email`, `password`) VALUES
-(1, 'Luke', 'Zhang', 'lukezhangxc', NULL, 'lukezhangxc@gmail.com', 'pbkdf2:sha256:150000$vvZrXBi9$8637f8983a43d4cff39735bdcc9417e2991703f9f6da9f3b3f701abe9c520146'),
-(2, 'James', 'Huang', 'jjhuang1', NULL, 'jjhuang1@gmail.com', 'pbkdf2:sha256:150000$5ybzFs5w$631f190f047d93b90b2680b7a091bae42f181c2dfec2dbc390a701ee5ca91d8c'),
-(3, 'Tom', 'Carsello', 'tkcarsello', NULL, 'tkcarsello@gmail.com', 'pbkdf2:sha256:150000$d93jkf8Y$bb9c18c3d39e245f96bb621aa228529a4a45801aae413461a5299b91bf8d5d63'),
-(4, 'Ethan', 'He', 'ehe', NULL, 'ehe@gmail.com', 'pbkdf2:sha256:150000$WH0lIkjz$35a64acdab8bff6a903a834e3e0977b604d32859525c170dc6eb5937f6806b50');
+(5, 'Luke', 'Zhang', 'lukezhang37', NULL, 'lukezhangxc@gmail.com', 'pbkdf2:sha256:150000$daCDKQt4$7bf1f690580110f9dcd14a24aa81005d5728ed9b7f69b6ce1c7d85bcb76ec2bd');
 
 -- --------------------------------------------------------
 
@@ -156,8 +142,8 @@ INSERT INTO `organizers` (`id`, `firstN`, `lastN`, `username`, `organizationID`,
 --
 
 CREATE TABLE `participants` (
-  `organizer_id` int(11) NOT NULL,
-  `activity_id` int(11) NOT NULL
+  `organizer_id` int NOT NULL,
+  `activity_id` int NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -165,14 +151,9 @@ CREATE TABLE `participants` (
 --
 
 INSERT INTO `participants` (`organizer_id`, `activity_id`) VALUES
-(1, 1),
-(1, 2),
-(1, 3),
-(2, 3),
-(2, 6),
-(3, 5),
-(3, 11),
-(4, 7);
+(5, 1),
+(5, 2),
+(5, 3);
 
 -- --------------------------------------------------------
 
@@ -181,12 +162,12 @@ INSERT INTO `participants` (`organizer_id`, `activity_id`) VALUES
 --
 
 CREATE TABLE `students` (
-  `id` int(11) NOT NULL,
+  `id` int NOT NULL,
   `firstN` varchar(255) DEFAULT NULL,
   `lastN` varchar(255) DEFAULT NULL,
   `targetID` varbinary(255) DEFAULT NULL,
-  `organizationID` int(11) DEFAULT NULL,
-  `isOrganizer` smallint(6) DEFAULT NULL
+  `organizationID` int DEFAULT NULL,
+  `isOrganizer` smallint DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -194,15 +175,15 @@ CREATE TABLE `students` (
 --
 
 INSERT INTO `students` (`id`, `firstN`, `lastN`, `targetID`, `organizationID`, `isOrganizer`) VALUES
-(1, 'Aditya', 'Tolia', NULL, NULL, NULL),
-(2, 'Robert', 'Azarcon', NULL, NULL, NULL),
-(3, 'Ethan', 'Pulvermacher', NULL, NULL, NULL),
-(4, 'Yinuo', 'Qin', NULL, NULL, NULL),
-(5, 'Vijay', 'Mistry', NULL, NULL, NULL),
-(6, 'Ishaan', 'Mathur', NULL, NULL, NULL),
-(71, 'Jack', 'Cook', NULL, NULL, NULL),
-(72, 'Nicholas', 'Williams', NULL, NULL, NULL),
-(75, 'Leo', 'Oberman', NULL, NULL, NULL);
+(1, 'James', 'Huang', NULL, NULL, NULL),
+(2, 'Tom', 'Carsello', NULL, NULL, NULL),
+(3, 'Ethan', 'He', NULL, NULL, NULL),
+(4, 'Enoch', 'Qin', NULL, NULL, NULL),
+(5, 'Yinuo', 'Qin', NULL, NULL, NULL),
+(6, 'Joshua', 'Robinson', NULL, NULL, NULL),
+(7, 'Omeed', 'Jamali', NULL, NULL, NULL),
+(8, 'Ishaan', 'Mathur', NULL, NULL, NULL),
+(9, 'James', 'Tom', NULL, NULL, NULL);
 
 --
 -- Indexes for dumped tables
@@ -262,11 +243,11 @@ ALTER TABLE `participants`
 --
 ALTER TABLE `students`
   ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `ix_students_isOrganizer` (`isOrganizer`),
-  ADD UNIQUE KEY `ix_students_organizationID` (`organizationID`),
   ADD UNIQUE KEY `ix_students_targetID` (`targetID`),
   ADD KEY `ix_students_firstN` (`firstN`),
-  ADD KEY `ix_students_lastN` (`lastN`);
+  ADD KEY `ix_students_lastN` (`lastN`),
+  ADD KEY `ix_students_isOrganizer` (`isOrganizer`),
+  ADD KEY `ix_students_organizationID` (`organizationID`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -276,31 +257,31 @@ ALTER TABLE `students`
 -- AUTO_INCREMENT for table `activity`
 --
 ALTER TABLE `activity`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 
 --
 -- AUTO_INCREMENT for table `assignments`
 --
 ALTER TABLE `assignments`
-  MODIFY `organizerID` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `organizerID` int NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `connection`
 --
 ALTER TABLE `connection`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=103;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=113;
 
 --
 -- AUTO_INCREMENT for table `organizers`
 --
 ALTER TABLE `organizers`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `students`
 --
 ALTER TABLE `students`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=76;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=76;
 
 --
 -- Constraints for dumped tables
